253f8d9220bffbea26581821740725fcb9dd4c19
Nicholas Kier <nicholas.k.kier@gmail.com>
2020-10-21 18:13:07 -0400

    Implementation of Slingshot:OnMouseEnter() and SlingshotOnMouseExit()
    Yeah!

---
+++ Mission Demolition Prototype/Assets/Scripts/Slingshot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    static public Slingshot Sling;
+
+    public GameObject prefabProjectile;
+    public float velocityMultiplier = 4.0F;
+    //public bool 
+
+    public GameObject launchPoint;
+    public Vector3 launchPosition;
+    public GameObject projectile;
+    public bool isAiming;
+
+    private void Awake()
+    {
+        Sling = this;
+        Transform launchPointTrans = transform.Find("Launchpoint");
+    }
+
+    private void OnMouseEnter()
+    {
+        print("Slingshot:OnMouseEnter()");
+    }
+    private void OnMouseExit()
+    {
+        print("Slingshot:OnMouseExit()");
+    }
+}
